394de087ba809058f3175f75532b298ad7ecc299
Matthias <tuttiacc@hotmail.com>
2023-12-03 16:51:39 +0100

    Updates

--- dist/index.js
+++ dist/index.js
@@ -10324,10 +10324,11 @@ let GoldenLayoutRoot = class GoldenLayoutRoot extends BaseElement {
         .lm_tabs {
             height: 50px;
             padding-left: 8px;
+            overflow-y: hidden;
         }
 
         .lm_tab {
-          height: 38px !important;
+          height: 40px !important;
           border-radius: 8px 8px 0 0;
           top: 8px;
           margin: 0 0 0 3px !important;
@@ -10340,7 +10341,7 @@ let GoldenLayoutRoot = class GoldenLayoutRoot extends BaseElement {
           color: black !important;
           max-width: 110px;
           position: relative !important;
-          background-color: white !important;
+          background: #22207457 !important;
         }
 
         .lm_tab::before {
@@ -10353,19 +10354,19 @@ let GoldenLayoutRoot = class GoldenLayoutRoot extends BaseElement {
             height: 40px !important;
             width: 15px !important;
             border-radius: 0 0 10px 0;
-            box-shadow: 0 10px 0 0 white;
+            box-shadow: 0 10px 0 0 rgb(149 157 206);
         }
 
         .lm_tab:hover::before {
-            box-shadow: 0 10px 0 0 rgb(149 157 206);
+            box-shadow: 0 10px 0 0 white;
         }
 
         .lm_tab:hover::after {
-            box-shadow: 0 10px 0 0 rgb(149 157 206);
+            box-shadow: 0 10px 0 0 white;
         }
 
         .lm_tab.lm_active::before {
-            box-shadow: 0 10px 0 0 rgb(149 157 206);
+            box-shadow: 0 10px 0 0 white;
         }
 
         /* .lm_tab.lm_active::after {
@@ -10382,20 +10383,28 @@ let GoldenLayoutRoot = class GoldenLayoutRoot extends BaseElement {
         } */
 
         .lm_tab:hover {
-            /* box-shadow: 0 0 1px #3d3d3d !important; */
-            background: rgb(149 157 206) !important;
+            box-shadow: 0 0 2px 2px #bfbfbf !important;
+            background: white !important;
+            color: #0e001c !important;
         }
 
         .lm_active {
-            box-shadow: 0 0 5px #3d3d3d !important;
-            background: rgb(149 157 206) !important;
-            color: black !important;
+            box-shadow: 0 0 2px 2px #bfbfbf !important;
+            background: white !important;
+            color: #0e001c !important;
         }
 
         .lm_header {
             height: 50px !important;
             background: #dbdef9;
             overflow-x: scroll !important;
+            overflow-y: hidden !important;
+            -ms-overflow-style: none; /* IE and Edge */
+            scrollbar-width: none; /* Firefox */
+        }
+
+        .lm_header::-webkit-scrollbar {
+          display: none;
         }
 
         .lm_close_tab {
